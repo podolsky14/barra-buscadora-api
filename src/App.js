e41a19a5df33cfc705585460be9e6b355add6978
Maximiliano Calero <93091450+podolsky14@users.noreply.github.com>
2022-02-17 17:54:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import "./index.css"
-
-
-
 import BuscadorApi from "./components/BuscadorApi";
-
-//import Ejemplo from "./components/Ejemplo";
-
 function App() {
-
-
-
-
-
-
-
-
   return (
     <>
     <BuscadorApi/>
-
-  
     </>
   );
 }
-
 export default App;
